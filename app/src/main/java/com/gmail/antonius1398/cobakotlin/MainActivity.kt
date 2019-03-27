641f79f4b2d17e4acaee3f8bfb6bcd06d06c3f03
antoniusjonatan <antonius1398@gmail.com>
2019-03-27 18:19:44 +0700

    Initial commit

---
+++ app/src/main/java/com/gmail/antonius1398/cobakotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gmail.antonius1398.cobakotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
